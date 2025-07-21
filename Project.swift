288bab77e347123983e21f57ea48b8f0ee8812e3
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2025-07-22 00:13:12 +0900

    Update Project.swift
    - Resources: Sources에 있는 Models 경로 추가

--- Project.swift
+++ Project.swift
@@ -10,26 +10,33 @@ let project = Project(
             bundleId: "io.tuist.safe",
             infoPlist: .extendingDefault(
                 with: [
-                    "UIMainStoryboardFile": "",
+                    // 카메라 접근 이유
+                    "NSCameraUsageDescription": "세잎 앱이 실시간 자세 추적을 위해 카메라를 사용합니다.",
+                    // 런치 스크린 기본값
                     "UILaunchScreen": [
                         "UIColorName": "",
                         "UIImageName": "",
                     ],
+                    // 씬 기반 라이프사이클 설정
                     "UIApplicationSceneManifest": [
                         "UIApplicationSupportsMultipleScenes": false,
                         "UISceneConfigurations": [
                             "UIWindowSceneSessionRoleApplication": [
                                 [
                                     "UISceneConfigurationName": "Default Configuration",
-                                    "UISceneDelegateClassName": "$(PRODUCT_MODULE_NAME).SceneDelegate"
+                                    "UISceneDelegateClassName": "$(PRODUCT_MODULE_NAME).SceneDelegate",
+
                                 ]
                             ]
                         ]
                     ]
                 ]
             ),
-            sources: ["Sources/**"],
-            resources: ["Resources/**"],
+            sources: ["safe/Sources/**"],
+            resources: [
+                "safe/Resources/**",
+                "safe/Sources/ML/Models/**"
+            ],
             dependencies: []
         ),
         .target(
@@ -38,7 +45,7 @@ let project = Project(
             product: .unitTests,
             bundleId: "io.tuist.safeTests",
             infoPlist: .default,
-            sources: ["Tests/**"],
+            sources: ["safe/Tests/**"],
             resources: [],
             dependencies: [.target(name: "safe")]
         ),
