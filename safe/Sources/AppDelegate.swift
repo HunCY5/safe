412d79ac279de7ad60a78a4856945e374799d8c7
= <leeh4743@naver.com>
2025-07-27 12:07:45 +0900

    Update Copyright Notice
    - 기존 MainTabBarController.swift , OverlayView 파일 상단에 저작권 관련 주석을 추가 및 수정함
    - 파일 작성자 및 생성일 정보를 명시하여 원본 소스에 대한 수정 이력을 명확히 표현

--- safe/Sources/AppDelegate.swift
+++ safe/Sources/AppDelegate.swift
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // =============================================================================
+// Modifications by Chansol Shin on 2025-07-22
 
 import UIKit
 import FirebaseCore
