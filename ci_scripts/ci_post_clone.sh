7c2318b7e67cc7424a78e4600778034b9950657d
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2026-02-27 20:11:12 +0900

    chore. Update ci_post_clone script
    - cd to repository root before tuist commands

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,6 +5,9 @@ curl https://mise.run | sh
 export PATH="$HOME/.local/bin:$PATH"
 
 mise use tuist@4.45.1 -g
+
+cd /Volumes/workspace/repository
+
 mise x tuist@4.45.1 -- tuist install
 mise x tuist@4.45.1 -- tuist generate
 
